035705f3cd473074498ef05a35c23b104bea132c
Ayush Ghimire <113327237+Aayush-jpg@users.noreply.github.com>
2023-04-01 01:34:50 -0400

    Update App.js
    This is the api from the browser request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Container from './components/Container';
 import Navbar from './components/Navbar';
+import Axios from 'axios'
 
 function App() {
+  const[joke,setjoke] = useState("");
+  const getjoke=()=> 
+  {
+    Axios.get("https://official-joke-api.appspot.com/randomjoke").then {
+    (response)=> {
+      console.log(response);
+      setjoke{response.data.setup + "..." + response.data.punchline);
+      
+  }}
   return (
     <>
       <Navbar />
       <div>
+    Hello <button onclick={getjoke}> Get Joke </button>
+    {joke}
         <Container />
       </div>
     </>
